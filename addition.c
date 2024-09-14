2c9d24df8bbbc57399e1fa2fc7f45c44e2aee81d
HenokTibebu <sasgedom8@gmail.com>
2024-09-14 15:32:47 +0300

    addition program

---
+++ addition.c
@@ -0,0 +1,14 @@
+//add two numbers user prompted
+#include <stdio.h>
+int main(){
+    int a , b , sum;
+    sum = a + b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("The sum of a and b is %d",sum);
+    return 0;
+
+}
