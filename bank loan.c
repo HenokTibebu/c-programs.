3122f9699ac31f88ae25731e8659eafb1875e431
HenokTibebu <sasgedom8@gmail.com>
2024-09-30 10:26:11 +0300

    Add files via upload

---
+++ bank loan.c
@@ -0,0 +1,15 @@
+//Bank loan program
+#include <stdio.h>
+int main(){
+    int age , income;
+    printf("Enter your age and income:");
+    scanf("%d%d",&age,&income);
+    if(age >= 21 && income >= 21000){
+        printf("Congratulations you qualify for a loan");
+    }
+    else{
+        printf("Unfortunately we are unable to offer you a loan at this time");
+    }
+
+    return 0;
+}
